b9169a8989a62ae387244187b2f6c21e4aeb85bf
MarvNC <rotrobmin@gmail.com>
2024-10-03 16:34:59 -0700

    Clean up code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import fs from "node:fs";
-import { stringify, generate, parse } from "csv";
-// import path
+import { parse } from "csv-parse";
 import path from "node:path";
 import mdToPdf from "md-to-pdf";
 
@@ -8,6 +7,8 @@ const CRN_REGEX = /CRN (\d+)/g;
 const IGNORE_NAMES = ["Student, Test"];
 const OUT_FOLDER = `output`;
 const WIDTH_SIGNATURE_COL = 135;
+
+// Read args
 
 const gradesCsvPath = Bun.argv[2];
 if (!gradesCsvPath) {
@@ -17,48 +18,44 @@ if (!gradesCsvPath) {
 
 const gradesCsvPathStr = gradesCsvPath as string;
 
-async function processFile(filePath: string) {
-  const sections = new Map<string, Set<string>>();
-  const parser = fs.createReadStream(filePath).pipe(
-    parse({
-      columns: true,
-    })
-  );
-  for await (const record of parser) {
-    const sectionText = record["Section"];
-    if (!sectionText) {
-      continue;
-    }
-    const CRNs = [...sectionText.matchAll(CRN_REGEX)].map((match) => match[1]);
-    if (CRNs.length === 0) {
-      continue;
-    }
-    const student = record["Student"];
-    for (const CRN of CRNs) {
-      const section = sections.get(CRN);
-      if (section) {
-        section.add(student);
-      } else {
-        const newSection = new Set<string>();
-        newSection.add(student);
-        sections.set(CRN, newSection);
-      }
+// Parse sections
+
+const sectionsMap = new Map<string, Set<string>>();
+const parser = fs.createReadStream(gradesCsvPathStr).pipe(
+  parse({
+    columns: true,
+  })
+);
+for await (const record of parser) {
+  const sectionText = record["Section"];
+  if (!sectionText) {
+    continue;
+  }
+  const CRNs = [...sectionText.matchAll(CRN_REGEX)].map((match) => match[1]);
+  if (CRNs.length === 0) {
+    continue;
+  }
+  const student = record["Student"];
+  for (const CRN of CRNs) {
+    const section = sectionsMap.get(CRN);
+    if (section) {
+      section.add(student);
+    } else {
+      const newSection = new Set<string>();
+      newSection.add(student);
+      sectionsMap.set(CRN, newSection);
     }
   }
-  return sections;
 }
 
-const records = await processFile(gradesCsvPathStr);
-
 // Create md files
+
 const mdFiles = [];
-for (const [CRN, students] of records) {
+for (const [CRN, students] of sectionsMap) {
   // Create an `attendance-${CRN}.md` file for each section
-  // const fileName = `attendance-${CRN}.md`;
   const fileName = path.join(OUT_FOLDER, `attendance-${CRN}.md`);
 
   let mdString = `# Attendance for CRN ${CRN}\n\n`;
-  // Table headers
   mdString += `| Student | Signature |\n| --- | --- |\n`;
   // Table rows
   for (const student of students) {
@@ -75,6 +72,7 @@ for (const [CRN, students] of records) {
 }
 
 // Generate PDFs
+
 for (const mdFile of mdFiles) {
   // Read the markdown file
   const mdContent = await Bun.file(mdFile).text();
